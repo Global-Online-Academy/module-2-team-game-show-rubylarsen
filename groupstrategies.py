b43b2a7423fc5b8143728abfde3066552c799bd8
Ruby Larsen <rubylarsen@Rubys-MacBook-Pro.local>
2025-02-14 16:35:03 -0600

    I (Ruby) was the driver for strategy 3 of our team's group

--- groupstrategies.py
+++ groupstrategies.py
@@ -20,4 +20,26 @@ hist4 = [("split","steal"),("steal","steal"),("split","steal"),("steal","split")
 
 # Your team's 3rd strategy (leave blank if you are not the driver)
 # Explanation of Strategy:
-# 
+# In this strategy the person will choose split for the first game if there has not been any prior to it, however if there has to take the percentage of times the player splits and if it is greater than or equal to .2 (20%), then it will steal
+
+def LuckisEverything3(history):
+    totalsplit = 0
+    if len(history) == 0:
+        return "split"
+    else:
+        for i in range (len(history)):
+            (me, them) = history[i]
+            if them == "split":
+                totalsplit = totalsplit + 1
+                if len(history) == 0:
+                    return "steal"
+                else:
+                    percentagesplit = totalsplit/len(history)
+
+                    if percentagesplit >= 0.20:
+                        return "steal"
+                    else:
+                        return "split"
+
+print(LuckisEverything3(history = hist3))
+
